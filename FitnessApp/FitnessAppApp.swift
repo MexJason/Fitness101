d8e258e4ab5918184359a0cfef05a9cb624553d5
Jason Dubon <jasonsdubon@gmail.com>
2023-09-10 09:24:52 -0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Course on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
